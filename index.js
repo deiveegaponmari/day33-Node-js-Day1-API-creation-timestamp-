147705d59f795fe3bc0c7a7948d087e0ed8055a9
deiveegaponmari <ponmarimdeiveega@gmail.com>
2024-11-27 20:33:21 +0530

    80% completed the task

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+const { error } = require('console');
 const express = require('express');
-const web_server=express();
-const {makediectory,createTextfile} =require('./utils/filesys');
-const {timestamp}=require('./utils/timestamp')
+const fs = require('fs');
+const path = require('path');
+const web_server = express();
+//folder to store file
+const folder=path.join(__dirname,'data');
+if(!fs.existsSync(folder)){
+    fs.mkdirSync(folder)
+}
+//API to create new text file
+web_server.post('/createfile',(req,res)=>{
+    const timestamp=new Date();
+    const filename=`${timestamp}`.txt;
+    const content=`current timestamp is: ${timestamp}`;
+    fs.writeFile(path.join(folder,filename),content,(err)=>{
+        if(err){
+            return res.status(500).json({error:"failed to create file"})
+        }
+        res.status(200).json({message:'file created',filename})
+    })
 
-web_server.get("/",(req,res)=>{
-    console.log("get value")
-    res.send("srikanth")
+});
+//API to retrieve all files
+web_server.get('/get-files',(res,req)=>{
+    fs.readdir(folder,(err,files)=>{
+        if(err){
+            return res.status(500).json({error:"failed to retrieve files"})
+        }
+        //filter any file
+        const textfiles=files.filter(file=>file.endsWith('.txt'))
+        res.status(200).json({files:textfiles})
+    })
 })
+
 web_server.listen(3000,"localhost",()=>{
-    console.log("server started",timestamp());
-    makediectory();
-    createTextfile();
+    console.log("server started");
+    console.log("http://localhost:3000/");
 })
